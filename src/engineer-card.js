4d9233268744387def8a9dde7b964138b773ba3a
Turbo Gordz <gordon.draine97@yahoo.com>
2022-08-20 21:05:40 -0400

    adding teammember structure and card strucutre

---
+++ src/engineer-card.js
@@ -0,0 +1,23 @@
+const engineerCard = (profile) => `
+  <div class="card col-md-4 p-0">
+    <div class="card-header bg-secondary text-white">
+      <h3> ${profile.getName()} </h3>
+      <h4> ${profile.getRole()} </h4>
+    </div>
+    <div class="card-body bg-light">
+      <ul class="list-group">
+        <li class="list-group-item">
+          <strong>ID:</strong> ${profile.getID()}
+        </li>
+        <li class="list-group-item">         
+          <strong>Email:</strong> <a href="mailto:${profile.getEmail()}">${profile.getEmail()}</a>
+        </li>
+        <li class="list-group-item">          
+          <strong>GitHub:</strong> <a href="http://www.github.com/${profile.getGithub()}" target="_blank">${profile.getGithub()}</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+`;
+
+module.exports = engineerCard;
